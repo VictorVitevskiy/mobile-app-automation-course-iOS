1cd05c594d25972d77df676490359ea54a3bbed6
Victor Vitevskiy <vitevskiy@mail.ru>
2021-10-13 11:27:31 +0300

    Ninth lesson. Second task.

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -86,30 +86,5 @@ public class SearchTests extends CoreTestCase {
         searchPageObject.assertThereIsNoResultOfSearch();
     }
 
-    @Test
-    public void testVerifyFirstThreeSearchResults() {
 
-        String search_text = "Java";
-
-        String first_result_title = "Java";
-        String second_result_title = "JavaScript";
-        String third_result_title = "Java (programming language)";
-        String first_result_description;
-        String second_result_description;
-        if (!Platform.getInstance().isMobileWeb()) {
-            first_result_description = "Island of Indonesia";
-            second_result_description = "Programming language";
-        } else {
-            first_result_description = "Indonesian island";
-            second_result_description = "High-level programming language";
-        }
-        String third_result_description = "Object-oriented programming language";
-
-        SearchPageObject searchPageObject = SearchPageObjectFactory.get(driver);
-        searchPageObject.initSearchInput();
-        searchPageObject.typeSearchLine(search_text);
-        searchPageObject.waitForElementByTitleAndDescription(first_result_title, first_result_description);
-        searchPageObject.waitForElementByTitleAndDescription(second_result_title, second_result_description);
-        searchPageObject.waitForElementByTitleAndDescription(third_result_title, third_result_description);
-    }
 }
