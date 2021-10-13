c4a59c3edbe8de50d9e7c41363a33faa649f0a98
Victor Vitevskiy <vitevskiy@mail.ru>
2021-10-13 15:57:48 +0300

    Ninth lesson. First task.

--- src/test/java/tests/MyListsTests.java
+++ src/test/java/tests/MyListsTests.java
@@ -61,5 +61,59 @@ public class MyListsTests extends CoreTestCase {
         myListsPageObject.swipeByArticleToDelete(article_title);
     }
 
+    @Test
+    public void testSaveTwoArticlesToMyList() throws InterruptedException {
 
+        String folder_name = "Learning programming";
+        String first_title = "Java (programming language)";
+        String second_title = "Python (programming language)";
+
+        SearchPageObject searchPageObject = SearchPageObjectFactory.get(driver);
+        searchPageObject.initSearchInput();
+        searchPageObject.typeSearchLine("Java");
+        searchPageObject.clickByArticleWithTitle(first_title);
+
+        ArticlePageObject articlePage = ArticlePageObjectFactory.get(driver);
+        if (Platform.getInstance().isAndroid()) {
+            articlePage.addArticleToMyListForTheFirstTime(folder_name);
+        } else if (Platform.getInstance().isIOS()){
+            articlePage.addArticlesToMySavedForTheFirstTime();
+        } else {
+            articlePage.addArticlesToMySaved();
+            Thread.sleep(1000);
+            AuthorizationPageObject authorizationPage = new AuthorizationPageObject(driver);
+            authorizationPage.clickAuthorizationButton();
+            authorizationPage.enterLoginData(LOGIN, PASSWORD);
+            authorizationPage.submitForm();
+
+            Assertions.assertEquals(
+                    first_title,
+                    articlePage.getArticleTitle(),
+                    "We are not on the same page after login."
+            );
+            articlePage.addArticlesToMySaved();
+        }
+        articlePage.closeArticle();
+
+        searchPageObject.initSearchInput();
+        searchPageObject.typeSearchLine("Python");
+        searchPageObject.clickByArticleWithTitle(second_title);
+
+        if (Platform.getInstance().isAndroid()) {
+            articlePage.addArticleIntoExistingMyList(folder_name);
+        } else {
+            articlePage.addArticlesToMySaved();
+        }
+        articlePage.closeArticle();
+
+        NavigationUI navigationUI = NavigationUIFactory.get(driver);
+        navigationUI.clickMyLists();
+
+        MyListsPageObject myListsPageObject = MyListsPageObjectFactory.get(driver);
+        if (Platform.getInstance().isAndroid()) {
+            myListsPageObject.openFolderByName(folder_name);
+        }
+        myListsPageObject.swipeByArticleToDelete(first_title);
+        myListsPageObject.waitForArticleToAppearByTitle(second_title);
+    }
 }
